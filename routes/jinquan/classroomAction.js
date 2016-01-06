8caef682338029775f0aa4b6bf7f3b73cc6bb59d
77665544332211 <764127745@qq.com>
2016-01-07 06:34:00 +0800

    增加教室管理列表

---
+++ routes/jinquan/classroomAction.js
@@ -0,0 +1,11 @@
+/**
+ * Created by kuanchang on 16/1/7.
+ */
+/**
+ * 获取教室列表
+ * @param req
+ * @param res
+ */
+module.exports.list = function (req, res) {
+    res.render('classroom/classroomList');
+}
--- routes/jinquan/index.js
+++ routes/jinquan/index.js
@@ -11,11 +11,13 @@ var shopAction = require('./shopAction');
 var storeroomAction = require('./storeroomAction');
 var waresAction = require('./waresAction');
 var classifyAction = require('./classifyAction');
+var classroomAction = require('./classroomAction');//教室管理
 
 /* GET home page. */
 router.all('/',welcomeAction.index);
 router.all('/menu', welcomeAction.Menu);
 router.all('/welcome',welcomeAction.Welcome);
+/*********************门店管理****************************/
 // 门店管理
 router.all('/shop_list',shopAction.list);
 router.all('/shop_pre_add',shopAction.preAdd);
@@ -25,6 +27,38 @@ router.all('/shop_set',shopAction.setStatus);
 router.all('/shop_pre_edit',shopAction.preEdit);
 router.all('/shop_update',shopAction.update);
 
+/*********************教室及课程管理****************************/
+// 教室管理
+router.all('/classroom_list',classroomAction.list);
+// 排课情况
+// 课程查询
+//router.all('/shop_list',shopAction.list);
+
+/*********************员工管理****************************/
+// 员工列表
+// 员工培训
+// 打卡记录
+// 考勤列别
+// 考勤变更
+// 查询考勤
+// 绩效查询
+// 绩效考勤
+
+/*********************会员管理****************************/
+// 会员卡类型
+// 会员卡管理
+// 活动管理
+// 会员列表
+// 预约服务
+// 课程报名
+// 护理服务
+// 收费信息
+// 回访信息
+// 投诉信息
+
+
+/*********************进销存管理***************************/
+// 服务管理
 // 商品管理
 router.all('/wares_list', waresAction.list);
 router.all('/wares_detail', waresAction.detail);
@@ -40,6 +74,13 @@ router.all('/storeroom_pre_add', storeroomAction.preAdd);
 router.all('/storeroom_set', storeroomAction.setStatus);
 router.all('/storeroom_del', storeroomAction.del);
 
+// 经销商管理
+// 入库管理
+// 出库管理
+// 移库管理
+// 盘点库存
+
+/*********************权限管理******************************/
 // 分类管理
 router.all('/main_classify_list', classifyAction.list);
 router.all('/main_classify_del', classifyAction.del);
@@ -53,5 +94,12 @@ router.all('/sub_classify_pre_add', classifyAction.preSubAddClassify);
 router.all('/sub_classify_add',classifyAction.addSubClassify);
 router.all('/sub_classify_pre_edit',classifyAction.preSubEdit);
 router.all('/sub_classify_update',classifyAction.subUpdate);
+
+// 资源按钮管理
+// 菜单管理
+// 角色管理
+// 系统用户管理
+// 个人设置
+
 
 module.exports = router;
