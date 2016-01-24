462ceccbd153912d9bfb43df587b508d0ee05411
wangjuan <472989858@qq.com>
2016-01-24 23:10:40 +0800

    编辑教室管理的增删改查

--- routes/jinquan/classroomAction.js
+++ routes/jinquan/classroomAction.js
@@ -6,8 +6,27 @@
  * @param req
  * @param res
  */
+var service = require('../../model/service/classroom');
+var waresService = require('../../model/service/wares');
 module.exports.list = function (req, res) {
-    res.render('classroom/classroomList');
+    var currentPage = req.query.page ? req.query.page : '1';
+    var className = req.query.className ? req.query.className : '';
+    var classCode = req.query.classCode ? req.query.classCode : '';
+    var classType = req.query.number ? req.query.number : '';
+
+    service.fetchAllCLassRoom(className,classCode,classType,currentPage, function (err, results) {
+        if (!err) {
+            results.currentPage = currentPage;
+            results.className = className;
+            results.classCode = classCode;
+            results.classType = classType;
+            res.render('classroom/classroomList', {data : results});
+        } else {
+            console.log(err.message);
+            res.render('error', {error : err});
+        }
+    });
+
 }
 
 /**
@@ -16,5 +35,22 @@ module.exports.list = function (req, res) {
  * @param res
  */
 module.exports.add = function (req, res) {
-    res.render('classroom/classroomAdd');
+    /*var currentPage = req.query.page ? req.query.page : '1';
+    var className = req.query.className ? req.query.className : '';
+    var classCode = req.query.classCode ? req.query.classCode : '';
+    var classType = req.query.number ? req.query.number : '';
+    waresService.fetchAllWares(currentPage,className,classCode,classType, function (err, results)
+    {
+        if (!err) {
+            results.currentPage = currentPage;
+            results.className = className;
+            results.classCode = classCode;
+            results.classType = classType;
+            res.render('classroom/classroomAdd', {datas : results});
+        } else {
+            console.log(err.message);
+            res.render('error', {error : err});
+        }
+    });*/
+    res.render('classroom/classroomAdd' , {datas : null});
 }
