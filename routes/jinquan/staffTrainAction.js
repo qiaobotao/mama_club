806e7d345e5834dafb7e3971a36aee0e2f937516
77665544332211 <764127745@qq.com>
2016-03-23 17:05:30 +0800

    解决页面中时间显示问题

--- routes/jinquan/staffTrainAction.js
+++ routes/jinquan/staffTrainAction.js
@@ -23,7 +23,8 @@ module.exports.list = function (req, res) {
             results.courseName = courseName;
             results.teacherName = teacherName;
             results.classroomName = classroomName;
-            res.render('staffTrain/staffTrainList', {data : results,replytype:replytype});
+            var courseDate = ['8:00','8:30','9:00','9:30','10:00','10:30','11:00','11:30','12:00','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00'];
+            res.render('staffTrain/staffTrainList', {data : results,replytype:replytype,courseDate:courseDate});
         } else {
             next();
         }
