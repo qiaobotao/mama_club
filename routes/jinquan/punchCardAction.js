c76d1beca08e5f01c3b62f8d32302fbfa07b4102
77665544332211 <764127745@qq.com>
2016-04-04 18:08:37 +0800

    分页采用http://laypage.layui.com这个 在打卡记录listAction中添加了代码，但是页面跳转进这方法后没往下进行，@乔 看能不能找到怎么回事

--- routes/jinquan/punchCardAction.js
+++ routes/jinquan/punchCardAction.js
@@ -2,6 +2,10 @@
  * Created by qiaojoe on 16-3-19.
  */
 var service = require('../../model/service/punchCard');
+
+var laypage = require('laypage');
+var router = require('express').Router();
+
 var multiparty = require('multiparty');
 var xlsx = require('node-xlsx');
 var fs = require('fs');
@@ -15,17 +19,36 @@ module.exports.list = function (req, res, next) {
 
     var replytype = req.query.replytype ? req.query.replytype : '';
 
-    service.list(name,date,currentPage, function(err, results) {
-
-        if (!err) {
-            results.currentPage = currentPage;
-            results.name = name;
-            results.date = date;
-            res.render('punchCard/punchCardList', {data : results,replytype : replytype});
-        } else {
-            next();
-        }
+    router.get('punch_card_list/page/:num', function(req, res){
+        res.render('punchCard/punchCardList', {
+            laypage: laypage({
+                curr: req.params.page || 1
+                ,url: req.url //必传参数，获取当前页的url
+                ,pages: 18 //分页总数你需要通过sql查询得到
+            })
+        })
     });
+    router.get('punchCard/punchCardList/page/:num', function(req, res){
+        res.render('punchCard/punchCardList', {
+            laypage: laypage({
+                curr: req.params.page || 1
+                ,url: req.url //必传参数，获取当前页的url
+                ,pages: 18 //分页总数你需要通过sql查询得到
+            })
+        })
+    });
+
+    //service.list(name,date,currentPage, function(err, results) {
+    //
+    //    if (!err) {
+    //        results.currentPage = currentPage;
+    //        results.name = name;
+    //        results.date = date;
+    //        res.render('punchCard/punchCardList', {data : results,replytype : replytype});
+    //    } else {
+    //        next();
+    //    }
+    //});
 }
 
 module.exports.preimport = function (req, res, next) {
